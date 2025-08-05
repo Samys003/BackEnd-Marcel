899b0febdeb643896ef0dd7babee260a7608138f
SAMARA ROLIM DOS SANTOS <25132563@CFP127ALUNO.LOCAL>
2025-08-05 11:23:54 -0300

    atividade de taxa de juros compostos

--- Aula 02/app.js
+++ Aula 02/app.js
@@ -53,6 +53,13 @@ NÃO  NOT  !
  
  */
 
+//Padrão de mensagens de erro da aplicação
+const MESSAGE_ERROR_EMPTY = 'Erro: Existem campos que não foram preenchidos.'
+
+const MESSAGE_ERROR_NOT_NUMBER = 'Erro: não é possivel calcular com a entrada de letras.'
+
+const MESSAGE_ERROR_OUT_OF_RANGE = 'ERRO: Os valores informados precisam ser entre 0 ate 10.'
+
 //Import da biblioteca que calcula a média
 var mediaEscolar = require('./modulo/media.js')
 
@@ -86,14 +93,14 @@ entradaDeDados.question('Digite o nome do aluno:', function (nome) {
 
                     // validação de tratamento de entrada vazia
                     if(nota1 == '' || nota2 == '' || nota3 == '' || nota4 == '' || nomeAluno == ''){
-                        console.log('Erro: Existem campos que não foram preenchidos. ')
+                        console.log(MESSAGE_ERROR_EMPTY)
 
                     }else if(isNaN(nota1) || isNaN(nota2)  || isNaN(nota3) || isNaN(nota4)){   
-                            console.log('Erro: não é possivel calcular com a entrada de letras')
+                            console.log(MESSAGE_ERROR_NOT_NUMBER)
 
                     }else if(Number(nota1) < 0 || Number(nota1) > 10 || Number(nota2) < 0 || Number(nota2) > 10 || 
                              Number(nota3) < 0 || Number(nota3) > 10 || Number(nota4) < 0 || Number(nota4) > 10 ){
-                        console.log('ERRO: Os valores informados precisam ser entre 0 ate 10')  
+                        console.log(MESSAGE_ERROR_OUT_OF_RANGE)  
 
                     }else{
 
